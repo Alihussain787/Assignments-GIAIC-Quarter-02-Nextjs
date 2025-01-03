722b51b60dd4a648f56faa9dc845bf1d39879675
Alihussain787 <alihussainsindhi787@gmail.com>
2025-01-03 21:34:46 +0500

    Blog-Website-NextJS

--- Milestone-3-Blog-Web/src/app/components/posts/[post]/page.tsx
+++ Milestone-3-Blog-Web/src/app/components/posts/[post]/page.tsx
@@ -38,18 +38,23 @@ const Post = ({params}:{params:{post:number}}) => {
 
   return (
     <div className='w-full flex flex-col items-center justify-center py-10 px-10 gap-4'>
-        <div className='w-1/2 flex flex-col bg-[#23A6F0] py-2 rounded'>
+        <div className='w-1/2 flex flex-col border py-2 rounded max-md:w-full'>
             <p className='px-4'>User ID: {data?.userId}</p>
             <div className='flex flex-col'>
                <div className='p-4 flex flex-col gap-2'>
-                <p>Title: {data?.title}</p>
-                <p className='text-gray-600'>{data?.body}</p>
+                  <p>Title: {data?.title}</p>
+                  <p className='text-gray-600'>{data?.body}</p>
+                </div>
+                <div className='flex gap-8 px-4 py-2'>
+                  <p>{data?.views} Views</p>
+                  <p>{data?.reactions.likes} Likes</p>
+                  <p>{data?.reactions.likes} Dislikes</p>
               </div>
-              <div className='items-center flex gap-4 py-2 px-4'>
-                <button onClick={() => setDisplay((prevDisplay) => !prevDisplay)} className="h-[32px]">Comment</button>
-                <div style={{ display: display ? "block" : "none" }} className="w-full relative flex items-center justify-center gap-4">
-                    <input type="text" placeholder="Comment..." onChange={(text) => setComment(text.target.value)} className="w-full h-[30px] outline-0"/>
-                    <button onClick={() => {setDisplay((prevDisplay) => !prevDisplay); printComment()}} className="text-white bg-green-500 w-[200px] h-[30px] absolute right-0">Submit</button>
+              <div className='w-full flex flex-col gap-4 py-2 px-4'>
+                <button onClick={() => setDisplay((prevDisplay) => !prevDisplay)} className="w-[200px] h-[32px] text-start">Click for comment</button>
+                <div style={{ display: display ? "block" : "none" }} className="border w-full relative flex items-center justify-center gap-4">
+                    <input type="text" placeholder="Text..." onChange={(text) => setComment(text.target.value)} className="w-full h-[30px] border outline-0"/>
+                    <button onClick={() => {setDisplay((prevDisplay) => !prevDisplay); printComment()}} className="text-white bg-[#23A6F0] w-[200px] h-[30px] absolute right-0">Submit</button>
                 </div>
               </div>
             </div>
